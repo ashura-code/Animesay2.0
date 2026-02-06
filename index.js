62c7b159b7e91c16c72ec7c474d7fc9f60268622
adhitya nantish <adhitya2k3@gmail.com>
2026-02-07 00:08:52 +0530

    changed the scripts to a seperate file

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+const getPhoto = async (name, anime) => {
+  const url = `https://real-time-image-search.p.rapidapi.com/search?query=${name + ' ' + anime}&limit=1&size=any&color=any&type=any&time=any&usage_rights=any&file_type=any&aspect_ratio=any&safe_search=off&region=us`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': 'cc7365826amsh843b939188aa9ebp14e9dajsn4316a1950472',
+      'x-rapidapi-host': 'real-time-image-search.p.rapidapi.com',
+    },
+  };
+
+  try {
+    console.log('came here');
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    document.getElementById('image_container').innerHTML =
+      `<img src="${result.data[0].url}"/>`;
+    console.log(result.data[0].url);
+    // console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
 const getAnimeQuote = async () => {
   let variable = await fetch('https://api.rei.my.id/animequotes/random')
     .then((val) => val.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      document.getElementById('quote').innerHTML = res.english
+        ? res.english
+        : JSON.stringify(res);
+      document.getElementById('character').innerHTML = res.character
+        ? '<b>character: </b>' + res.character
+        : JSON.stringify(res);
+      document.getElementById('anime').innerHTML = res.anime
+        ? res.anime
+        : JSON.stringify(res);
+      // getPhoto(res.character, res.anime);
+    });
 };
 
+getAnimeQuote();
